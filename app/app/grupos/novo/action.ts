800605b46e7cc87f7080f2ee75fdd741cfbfb52a
Guilherme Miyata <miyatagui120600@gmail.com>
2025-03-13 18:06:56 -0300

    feat: add function to post new participants to the database and assign each one to another random participant

--- app/app/grupos/novo/action.ts
+++ app/app/grupos/novo/action.ts
@@ -1,11 +1,30 @@
 'use server';
 import { createClient } from '@/utils/supabase/server';
+import { redirect } from 'next/navigation';
 
 export type CreateGroupState = {
   success: null | boolean;
   message?: string;
 };
 
+type Participant = {
+  id: string;
+  group_id: string;
+  name: string;
+  email: string;
+  assigned_to: string | null;
+  created_at: string;
+};
+function drawGroup(participants: Participant[]) {
+  const selectedParticipants: string[] = [];
+
+  return participants.map((participant) => {
+    const availableParticipants = participants.filter((p) => p.id !== participant.id && !selectedParticipants.includes(p.id));
+    const assignedParticipant = availableParticipants[Math.floor(Math.random() * availableParticipants.length)];
+    selectedParticipants.push(assignedParticipant.id);
+    return { ...participant, assigned_to: assignedParticipant.id };
+  });
+}
 export async function createGroup(_previousState: CreateGroupState, formData: FormData) {
   const supabase = await createClient();
   const { data: authUser, error: authError } = await supabase.auth.getUser();
@@ -18,7 +37,49 @@ export async function createGroup(_previousState: CreateGroupState, formData: Fo
   }
 
   const names = formData.getAll('name');
-  const email = formData.getAll('email');
+  const emails = formData.getAll('email');
   const groupName = formData.get('group-name');
-  console.log(names, email, groupName);
+
+  const { data: newGroup, error } = await supabase
+    .from('groups')
+    .insert({
+      name: groupName,
+      owner_id: authUser?.user.id,
+    })
+    .select()
+    .single();
+
+  if (error) {
+    return {
+      success: false,
+      message: 'Ocorreu um erro ao criar o grupo! Por favor tente novamente.',
+    };
+  }
+
+  const participants = names.map((name, index) => ({
+    group_id: newGroup.id,
+    name,
+    email: emails[index],
+  }));
+
+  const { data: createdParticipants, error: errorParticipants } = await supabase.from('participants').insert(participants).select();
+
+  if (errorParticipants) {
+    return {
+      success: false,
+      message: 'Ocorreu um erro ao adicionar os participantes ao grupo! Por favor tente novamente.',
+    };
+  }
+
+  const drawParticipants = drawGroup(createdParticipants);
+  const { error: errorDraw } = await supabase.from('participants').upsert(drawParticipants);
+
+  if (errorDraw) {
+    return {
+      success: false,
+      message: 'Ocorreu um erro ao sorter os participantes do grupo! Por favor tente novamente.',
+    };
+  }
+  // redirect(`app/grupos/${newGroup.id}`);
 }
+
